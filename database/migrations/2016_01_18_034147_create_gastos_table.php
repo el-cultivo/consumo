c02794ae1646a294e04775ffb9ed216706e35404
rbto <robertopalomino87@gmail.com>
2016-01-17 21:53:44 -0600

    Added: Gasto model

---
+++ database/migrations/2016_01_18_034147_create_gastos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGastosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gastos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre');
+            $table->decimal('cantidad',10, 2);
+            $table->integer('tipo_pago');
+            $table->integer('user_id')->unsigned();
+            $table->timestamps();
+
+            $table  ->foreign('user_id')
+                    ->references('id')
+                    ->on('users')
+                    ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('gastos');
+    }
+}
