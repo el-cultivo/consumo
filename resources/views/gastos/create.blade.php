77ec21efb1140079f9cb0c7ef5f0fedea2f54bbf
rbto <robertopalomino87@gmail.com>
2016-02-06 18:18:35 -0600

    Added: logout link

--- resources/views/gastos/create.blade.php
+++ resources/views/gastos/create.blade.php
@@ -26,8 +26,9 @@
             <ul>
                 <li><a href="#">Presupuestos</a></li>
                 <li><a href="#">Gastos</a></li>
-                {{-- <li><a href="#">Deudas</a></li>
-                <li><a href="#">Config</a></li> --}}
+                <li><a href="#">Deudas</a></li>
+                <li><a href="{{URL::to('auth/logout')}}"><small>Salir</small></a></li>
+                {{-- <li><a href="#">Config</a></li> --}}
             </ul>
         </div>
         <div class="col-md-3 informacion">
